153e082b9506a766376fc526d240a6e145d89acd
WilliamWinterman <48756260+WilliamWinterman@users.noreply.github.com>
2020-12-06 15:43:29 +0200

    Update WebService.js

--- WebService.js
+++ WebService.js
@@ -4,13 +4,13 @@ app.disable('etag'); //will return 200 http status code on success
 var counter = 0
 
 // GET method route, when recieved a get request it will respond with the counter of post requests.
-app.get('/getCounter', function (req, res) {
+app.get('/getRequest', function (req, res) {
   res.send("Post Counter: " + counter)
   console.log(counter);
   })
   
 // POST method route
-app.post('/postCounter', function (req, res) {
+app.post('/', function (req, res) {
     counter++
 
   })
