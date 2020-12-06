4ce13c7a87c67ffaab17981a16611375559acbc3
WilliamWinterman <48756260+WilliamWinterman@users.noreply.github.com>
2020-12-06 13:27:43 +0200

    Update WebService.js

--- WebService.js
+++ WebService.js
@@ -1,9 +1,9 @@
 var express = require('express')
 var app = express()
-app.disable('etag');
+app.disable('etag'); //will return 200 http status code instead of 302
 var counter = 0
 
-// GET method route, when recieved a get request it will respond with the 
+// GET method route, when recieved a get request it will respond with the counter of post requests.
 app.get('/getCounter', function (req, res) {
   res.send({data: counter})
   console.log(counter);
