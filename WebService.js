e37eecc22cb1c1e160c359149923c891de96d324
WilliamWinterman <48756260+WilliamWinterman@users.noreply.github.com>
2020-12-06 10:31:46 +0200

    Update WebService.js

--- WebService.js
+++ WebService.js
@@ -1,16 +1,17 @@
 var express = require('express')
 var app = express()
+app.disable('etag');
 var counter = 0
 
-// GET method route
+// GET method route, when recieved a get request it will respond with the 
 app.get('/getCounter', function (req, res) {
   res.send({data: counter})
+  console.log(counter);
   })
   
 // POST method route
 app.post('/postCounter', function (req, res) {
     counter++
+
   })
-
-  app.listen(3000, "127.0.0.1")
-
+  app.listen(3000)
